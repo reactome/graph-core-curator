13f08d9030b44db0f5c80be97b9b43fef75f4108
Robert Petryszak <info@datasome.co.uk>
2022-04-15 23:17:03 +0100

    Made constraint() method in ReactomeConstraint public so that CuratorTool can access the value of the constraint

--- src/main/java/org/reactome/server/graph/curator/domain/annotations/ReactomeConstraint.java
+++ src/main/java/org/reactome/server/graph/curator/domain/annotations/ReactomeConstraint.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ReactomeConstraint {
-    Constraint constraint() default Constraint.OPTIONAL;
+    public Constraint constraint() default Constraint.OPTIONAL;
 
      enum Constraint {
         NOMANUALEDIT,
